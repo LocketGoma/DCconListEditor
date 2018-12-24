7fa1cbe13b0a2315466cf733d457f32fc9d76c14
LocketGoma <gom@kiwing.co.kr>
2018-12-24 21:54:23 +0900

    libiconv 추가, 리스트/엔트리 파싱 완료.

--- Addon_BBCC_DCconEditor_AUTO/FileEditor.cpp
+++ Addon_BBCC_DCconEditor_AUTO/FileEditor.cpp
@@ -1,6 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
 #include "FileEditor.h"
+#include "addition/include/iconv.h"
+
+
 
 
 /*'dccon_list.js 파일을 읽고, 수정하는 목적.*/
@@ -12,72 +15,69 @@ string default_path = "lib/dccon_list.js";	//기본 경로
 unique_ptr<fstream> dcconlist = make_unique<fstream>(default_path);
 
 
-void entry_parser() {
+
+
+void entry_maker() {
 	setlocale(LC_ALL, "korean");
 	dcconlist->seekg(16);
-	string temp;
-	
-	getline(*dcconlist, temp);
-	cout << temp << endl;
-	wchar_t cstr[256];
-	char sstr[256] = {};
-	MultiByteToWideChar(CP_UTF8, 0, temp.c_str(), -1, cstr, std::size(cstr));
 
-	wstring bgs(cstr);
-	string test;
 
-	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-	test = converter.to_bytes(bgs);
-
-	string example = "40K극혐.png";
-	cout << example.size() << endl;
-
-	int i = 0;
-	int j = 0;
-	while (cstr[i] != '\0') {
-		if (cstr[i] <= 0x7F) {
-			sstr[j] = cstr[i];
-			j++;
-		}
-		else {
-			sstr[j] = 0x80 | ((cstr[i] >> 6) & 0x39);
-			sstr[j + 1] = 0xD8 | (cstr[i] >> 6) ;
-
-			j += 2;
-		}
-		i++;
+	bool locker = true;
+	while (locker==true) {
+		locker = entry_parser();
 	}
-	sstr[j] = '\0';
-
-	cout << sstr << endl;
-	wcout << bgs << endl;
-	//00111111 (=3F)
-
-	//ADF9 -> B1 / D8
-	//1010110111111001
-	//1010110111 (6 shift)
-	//10110111 (=B7) // | 0x80
-
-	//10110001 (=B1)
-	//11011000 (=D8)
-
-	//D610 -> C7/F8
-	//1101011000010000
-	//1101011000 (6 shift)
-	//11011000 (=D8)// | 0x80
-	
-	//11000111 (=C7)
-	//11111000 (=F8)
-
-
 }
+bool entry_parser() {
+
+	string input;
+	string midpoint;
+	string output;
+	getline(*dcconlist, input);
+	if (dcconlist->fail() == true)
+		return false;
+
+	midpoint = convert_to_cp949(input.c_str());
+	cout << midpoint << endl;
+
+	if (midpoint.find("name") == string::npos)
+		return false;
+
+	output = midpoint.substr(midpoint.find("name") + 6, midpoint.find(""",")-(midpoint.find("name") + 7));
+	cout << output << endl;
+
+	entry_list.insert(pair<string, int>(output, 0));
+
+	return true;
+}
+string convert_to_cp949(const char * input) {
+	iconv_t changer;
+	changer = iconv_open("CP949", "UTF-8");
+
+	char deString[256] = { 0, };
+	char * dest = deString;
+
+	const char * test = input;
+
+	size_t stLen = (strlen(input) + 1) * sizeof(char);
+	size_t dsLen = 255;
+
+	if (changer != (iconv_t)(-1))
+		iconv(changer, &test, &stLen, &dest, &dsLen);
+
+	string answer(deString);
+
+	iconv_close(changer);
+
+	return answer;
+}
+
 
 
 
 
 #ifdef DEBUG
 void entry_test() {
-	entry_parser();
+	entry_maker();
 	
 	string temp;
 
@@ -102,6 +102,67 @@ void entry_test() {
 	//cout << temp << endl;
 
 	
+}
+
+void dump()			//쓸지말지 버릴지 안버릴지 아직 못정한것들. 
+{
+	setlocale(LC_ALL, "korean");
+	dcconlist->seekg(16);
+	string temp;
+
+	getline(*dcconlist, temp);
+	cout << temp << endl;
+	wchar_t cstr[256];
+	char sstr[256] = {};
+	MultiByteToWideChar(CP_UTF8, 0, temp.c_str(), -1, cstr, std::size(cstr));
+
+	wstring bgs(cstr);
+	string test;
+
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	test = converter.to_bytes(bgs);
+
+	string example = "40K극혐.png";
+	cout << example.size() << endl;
+	
+	
+	int i = 0;
+	int j = 0;
+	while (cstr[i] != '\0') {
+		if (cstr[i] <= 0x7F) {
+			sstr[j] = cstr[i];
+			j++;
+		}
+		else {
+			sstr[j] = 0x80 | ((cstr[i] >> 6) & 0x39);
+			sstr[j + 1] = 0xD8 | (cstr[i] >> 6);
+
+			j += 2;
+		}
+		i++;
+	}
+	sstr[j] = '\0';
+
+	cout << sstr << endl;
+	wcout << bgs << endl;
+	//00111111 (=3F)
+
+	//ADF9 -> B1 / D8
+	//1010110111111001
+	//1010110111 (6 shift)
+	//10110111 (=B7) // | 0x80
+
+	//10110001 (=B1)
+	//11011000 (=D8)
+
+	//D610 -> C7/F8
+	//1101011000010000
+	//1101011000 (6 shift)
+	//11011000 (=D8)// | 0x80
+
+	//11000111 (=C7)
+	//11111000 (=F8)
+
 }
 
 #endif // DEBUG
--- Addon_BBCC_DCconEditor_AUTO/FileEditor.h
+++ Addon_BBCC_DCconEditor_AUTO/FileEditor.h
@@ -8,7 +8,11 @@
 #include <vector>
 
 
-void entry_parser();
+void entry_maker();
+bool entry_parser();							//while문 안의 반복 함수
+string convert_to_cp949(const char * input);	//UTF-8 to CP949
+
+
 
 #ifdef DEBUG
 void entry_test();
