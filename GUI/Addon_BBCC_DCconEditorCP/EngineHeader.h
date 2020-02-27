901cf0d5aa3fd6da3a4b41b5d681d08b5e8d6f79
Locketgoma <gom@kiwing.co.kr>
2020-02-27 22:13:10 +0900

    GUI 그룹 업데이트. 개발 완료시 CUI 대체

---
+++ GUI/Addon_BBCC_DCconEditorCP/EngineHeader.h
@@ -0,0 +1,6 @@
+#pragma once
+//사용될 공용 헤더들
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
---
+++ GUI/Addon_BBCC_DCconEditorCP/FileListReader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "EngineHeader.h"
+//타 프로그램에서도 쓸 수 있는 구조로 작성할것.
+//C++ 17에서 작동함.
+#include <iostream>
+#include <filesystem>
+#include <typeinfo>
+#include <string>
+
+#if __cplusplus >= 201703L
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
+class FileListReader
+{
+	public:
+		void ListReader();
+		void ListReader(std::string path);
+		void ListPrinter();
+
+	private :
+		const std::string valid_ext_list[3] = { ".jpg",".gif",".png" };
+		std::map <std::string, bool> fileList;
+		std::map <std::string, bool>::iterator fileListIter;
+		
+		bool FileListReadingEngine(std::string path);
+		
+
+};
+
