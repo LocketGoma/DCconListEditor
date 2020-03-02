643be280fd129e9fe55a6326b9014eb090b319dd
Locketgoma <gom@kiwing.co.kr>
2020-03-02 17:41:19 +0900

    list file reader

--- GUI/Addon_BBCC_DCconEditorCP/BBCCListFileReader.cpp
+++ GUI/Addon_BBCC_DCconEditorCP/BBCCListFileReader.cpp
@@ -1,1 +1,50 @@
 #include "BBCCListFileReader.h"
+
+bool BBCCListFileReader::LinkingList(std::string route) {
+	std::string path = {};
+	if (route.empty() == true) {
+		path = "lib/dccon_list.js";
+	}
+	else {
+		path = route;
+	}
+
+	if (LoadEntry(path) == false) {
+		std::cout << "리스트 파일의 경로나 파일이 잘못되었습니다." << std::endl;
+		return false;
+	}
+
+}
+
+bool BBCCListFileReader::LoadEntry(std::string path) {
+	std:setlocale(LC_ALL, "korean");
+
+	try {
+		dcconEntry = std::make_unique<std::fstream>(path);
+	}
+	catch (std::exception e){
+		return false;
+	}
+
+	std::string testString;
+	std::getline(*dcconEntry, testString);
+	if (testString.substr(0, 10) == "dcConsData") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool BBCCListFileReader::ParseEntry() {
+	if (dcconEntry == nullptr) {
+		return false;
+	}
+	dcconEntry->seekg(16);
+
+	std::string tempInput;
+	std::string tempMidData;
+	std::string tempOutput;
+
+
+}
--- GUI/Addon_BBCC_DCconEditorCP/BBCCListFileReader.h
+++ GUI/Addon_BBCC_DCconEditorCP/BBCCListFileReader.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <fstream>
+#include <locale>
+#include <cstdio>
+#include "EngineHeader.h"
+
 class BBCCListFileReader
 {
+public:
+	bool LinkingList(std::string route);
+
+
+
+private:
+	std::map <std::string, bool> entryList;
+	std::map <std::string, bool>::iterator entryListIter;
+	std::unique_ptr<std::fstream> dcconEntry;
+
+	bool LoadEntry(std::string);
+	bool ParseEntry();
+	std::string ConvertToCP949(std::string input);
+	std::string ConvertToUTF8(std::string input);
+	std::string ConvertInputManager(std::string input);
+
 };
 
